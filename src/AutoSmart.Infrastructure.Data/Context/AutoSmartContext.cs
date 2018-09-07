70ce1f6017a3438703709cb6308a7ed6bebac8cc
Jonas Lima <jonas1307@live.com>
2018-09-07 20:57:29 -0300

    Inicializando contexto e camada de repositório

---
+++ src/AutoSmart.Infrastructure.Data/Context/AutoSmartContext.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+
+namespace AutoSmart.Infrastructure.Data.Context
+{
+    public class AutoSmartContext : DbContext
+    {
+        #region Constructors
+
+        public AutoSmartContext()
+            : base("AutoSmart")
+        { }
+
+        #endregion
+
+        #region Properties
+        #endregion
+
+        #region Methods
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // remove pluralizing and cascade deletion conventions
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            // string properties
+            modelBuilder.Properties<string>().Configure(p => p.HasColumnType("VARCHAR"));
+            modelBuilder.Properties<string>().Configure(p => p.HasMaxLength(255));
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataCadastro") != null))
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Property("DataCadastro").CurrentValue = DateTime.Now;
+                        break;
+                    case EntityState.Modified:
+                        entry.Property("DataCadastro").IsModified = false;
+                        break;
+                }
+            }
+
+            foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataModificacao") != null))
+            {
+                if (entry.State == EntityState.Modified)
+                {
+                    entry.Property("DataModificacao").IsModified = true;
+                    entry.Property("DataModificacao").CurrentValue = DateTime.Now;
+                }
+            }
+
+            return base.SaveChanges();
+        }
+
+        #endregion
+    }
+}
---
+++ src/AutoSmart.Infrastructure.Data/Repositories/RepositoryBase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using AutoSmart.Domain.Interfaces.Repositories;
+using AutoSmart.Infrastructure.Data.Context;
+
+namespace AutoSmart.Infrastructure.Data.Repositories
+{
+    public class RepositoryBase<TEntity> : IDisposable, IRepositoryBase<TEntity> where TEntity : class
+    {
+        private readonly AutoSmartContext _context = new AutoSmartContext();
+
+        public void Add(TEntity obj)
+        {
+            _context.Set<TEntity>().Add(obj);
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity obj)
+        {
+            _context.Entry(obj).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Remove(TEntity obj)
+        {
+            _context.Set<TEntity>().Remove(obj);
+            _context.SaveChanges();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
