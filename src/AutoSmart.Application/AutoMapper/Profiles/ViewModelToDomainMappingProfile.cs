6f43ff76735dddeb1b621d0ac5fa094bc642977a
Jonas Lima <jonas1307@live.com>
2018-09-07 23:02:42 -0300

    Adicionando Mappings

--- src/AutoSmart.Application/AutoMapper/Profiles/ViewModelToDomainMappingProfile.cs
+++ src/AutoSmart.Application/AutoMapper/Profiles/ViewModelToDomainMappingProfile.cs
@@ -1,10 +1,14 @@
 ﻿using AutoMapper;
+using AutoSmart.Application.ViewModel;
+using AutoSmart.Domain.Entities;
 
 namespace AutoSmart.Application.AutoMapper.Profiles
 {
     public class ViewModelToDomainMappingProfile : Profile
     {
         public ViewModelToDomainMappingProfile()
-        { }
+        {
+            CreateMap<ClienteViewModel, Cliente>();
+        }
     }
 }
