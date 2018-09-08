38799f7eb42fe83cbd5c6eaf40d8f189b4d173d4
Jonas Lima <jonas1307@live.com>
2018-09-07 21:07:03 -0300

    Implementando IoC/DI

---
+++ src/AutoSmart.Infrastructure.CrossCutting.IoC/Bootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using AutoSmart.Application.AppService;
+using AutoSmart.Application.Interfaces;
+using AutoSmart.Domain.Interfaces.Repositories;
+using AutoSmart.Domain.Interfaces.Services;
+using AutoSmart.Domain.Services;
+using AutoSmart.Infrastructure.Data.Repositories;
+using SimpleInjector;
+
+namespace AutoSmart.Infrastructure.CrossCutting.IoC
+{
+    public class Bootstrapper
+    {
+        public static void RegisterServices(Container container)
+        {
+            // App Services
+            container.Register(typeof(IAppServiceBase<>), typeof(AppServiceBase<>), Lifestyle.Scoped);
+
+            // Services
+            container.Register(typeof(IServiceBase<>), typeof(ServiceBase<>), Lifestyle.Scoped);
+
+            // Repositories
+            container.Register(typeof(IRepositoryBase<>), typeof(RepositoryBase<>), Lifestyle.Scoped);
+        }
+    }
+}
