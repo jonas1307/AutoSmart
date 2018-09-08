d4d30ccfed66d1ceefa88ee03c65ef0ad923386c
Jonas Lima <jonas1307@live.com>
2018-09-08 00:49:56 -0300

    Adicionando Display Name e Inicializador

--- src/AutoSmart.Application/ViewModel/ClienteViewModel.cs
+++ src/AutoSmart.Application/ViewModel/ClienteViewModel.cs
@@ -5,24 +5,34 @@ namespace AutoSmart.Application.ViewModel
 {
     public class ClienteViewModel
     {
+        public ClienteViewModel()
+        {
+            Id = 0;
+            PessoaFisica = true;
+            DataCriacao = null;
+        }
+
         [Key]
         public long Id { get; set; }
 
         [MinLength(3)]
         [MaxLength(255)]
+        [Display(Name = "Nome:")]
         [Required(ErrorMessage = "O campo é obrigatório.")]
         public string Nome { get; set; }
 
         public bool PessoaFisica { get; set; }
 
+        [Display(Name = "CPF/CNPJ:")]
         [Required(ErrorMessage = "O campo é obrigatório.")]
         public string CpfCnpj { get; set; }
 
         [MaxLength(14)]
+        [Display(Name = "Telefone:")]
         public string TelefoneContato { get; set; }
 
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
-        public DateTime DataCriacao { get; set; }
+        public DateTime? DataCriacao { get; set; }
     }
 }
