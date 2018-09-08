ef13e9a9f8df28e7bdcc84da853f6966676ae301
Jonas Lima <jonas1307@live.com>
2018-09-07 23:01:31 -0300

    Adicionando ViewModel Cliente

---
+++ src/AutoSmart.Application/ViewModel/ClienteViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AutoSmart.Application.ViewModel
+{
+    public class ClienteViewModel
+    {
+        [Key]
+        public long Id { get; set; }
+
+        [MinLength(3)]
+        [MaxLength(255)]
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public string Nome { get; set; }
+
+        public bool PessoaFisica { get; set; }
+
+        [Required(ErrorMessage = "O campo é obrigatório.")]
+        public string CpfCnpj { get; set; }
+
+        [MaxLength(14)]
+        public string TelefoneContato { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
+        public DateTime DataCriacao { get; set; }
+    }
+}
