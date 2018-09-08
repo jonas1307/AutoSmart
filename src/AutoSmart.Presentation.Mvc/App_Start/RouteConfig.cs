e42ca9ee9212136849ff2bfed20928fafabe442a
Jonas Lima <jonas1307@live.com>
2018-09-07 21:46:41 -0300

    Adicionando projeto Web

---
+++ src/AutoSmart.Presentation.Mvc/App_Start/RouteConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AutoSmart.Presentation.Mvc
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "DefaultRoute",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ src/AutoSmart.Presentation.Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutoSmart.Presentation.Mvc.MvcApplication" Language="C#" %>
