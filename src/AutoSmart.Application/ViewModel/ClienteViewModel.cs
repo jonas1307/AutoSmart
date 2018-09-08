e1afa72f6f0813cf2bb25986731a16d1d9649fac
Jonas Lima <jonas1307@live.com>
2018-09-08 01:17:45 -0300

    Exibição de Data de Criação

--- src/AutoSmart.Application/ViewModel/ClienteViewModel.cs
+++ src/AutoSmart.Application/ViewModel/ClienteViewModel.cs
@@ -32,6 +32,7 @@ namespace AutoSmart.Application.ViewModel
         public string TelefoneContato { get; set; }
 
         [DataType(DataType.Date)]
+        [Display(Name = "Data de Criação:")]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm:ss}")]
         public DateTime? DataCriacao { get; set; }
     }
